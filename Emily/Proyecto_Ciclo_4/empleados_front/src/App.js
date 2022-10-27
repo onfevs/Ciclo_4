4849f80d4f9ab29e7313100e105ad7cccc93b9d9
OnfeVS <jorgevallejo2990@gmail.com>
2022-10-27 14:49:25 -0500

    agregando iconos al login

--- Emily/Proyecto_Ciclo_4/empleados_front/src/App.js
+++ Emily/Proyecto_Ciclo_4/empleados_front/src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';// Importar desde boostrap
 import { Container } from 'react-bootstrap'; // Se importar los componentes en una sola linea con el {} separandolos de las comas,
 import Login from './components/login/login'; // Siempre en mayuscula la primera letra
 
-
 function App() {
 	return (
 		<div className="App">
--- Emily/Proyecto_Ciclo_4/empleados_front/src/components/login/login.js
+++ Emily/Proyecto_Ciclo_4/empleados_front/src/components/login/login.js
@@ -1,5 +1,10 @@
 import React from 'react'; // IMR
 import { Container, Form, FloatingLabel, Button, Row, Col } from 'react-bootstrap';
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import { MdPassword } from "react-icons/md";
+import { FcBusinessman } from "react-icons/fc";
+
+
 
 export default class login extends React.Component {
 	constructor(props) {
@@ -21,29 +26,29 @@ export default class login extends React.Component {
 				<Row>
 					<Col>
 						<Row>
-							<h2 style={{ marginBottom: 20 }}>Iniciar Sesion</h2>
+							<h2 style={{ marginBottom: 20, color: 'deepskyblue' }}>Iniciar Sesion</h2>
 						</Row>
-						<Row>
+						<Row><Form.Label style={{ alignItems: "center", display: "flex", gap: 10 }}><FcBusinessman /></Form.Label>
 							<FloatingLabel
 								controlId="floatingInput"
 								label="Usuario"
 								className="mb-4">
 								<Form.Control onChange={(e) => this.setState({ usuario: e.target.value })} required placeholder="Usuario" />
 							</FloatingLabel>
-							{/* <Form.Label >Password</Form.Label> */}
+							<Form.Label style={{ alignItems: "center", display: "flex", gap: 10 }}><MdPassword /></Form.Label>
 							<FloatingLabel controlId="floatingPassword" label="Contraseña" >
 								<Form.Control type="password" onChange={(e) => this.setState({ pass: e.target.value })} required placeholder="Password" />
 							</FloatingLabel>
-							<Button variant="primary" style={{ marginTop: 20, width: '100%' }}
+							<Button variant="primary" style={{ marginTop: 20, width: '100%', outline: '2px solid dodgerblue', outlineOffset: '2px', }}
 								onClick={() => {
 									this.iniciarSesion();
 								}}>
-								Iniciar Sesion
+								<HiArrowRightOnRectangle /> Iniciar Sesion
 							</Button>
 						</Row>
 					</Col>
-				</Row>
-			</Container>
+				</Row >
+			</Container >
 		);
 	}
 } // CCC 
